9451c41964befba9c068fe567b1daa4895256022
clarkdale123 <clarkdale123@yahoo.com>
2020-04-16 17:52:50 +0100

    Prime Number Calculator

--- prime.cpp
+++ prime.cpp
@@ -6,7 +6,34 @@ int if_prime(unsigned long long int x);
 
 int main()
 {
-unsigned long long int i,a=1;
-for(i=0;
-return 0;
+	int p=0;	
+	unsigned long long int a=1;
+	for(a=1; a>=0 ; a++)
+	{
+		p+=1;
+		p = if_prime(a);
+		if (p <= 2 && p != 0)
+		{
+			cout<<a<<endl;
+		}
+	}
+	return 0;
+}
+
+int if_prime(unsigned long long int x)
+{
+  unsigned long long int i;
+  int c=0;
+  for(i=1; i<=x; i++)
+  {
+  	if (x%i == 0)
+	{
+		c+=1;	
+	}
+	else
+	{
+		continue;
+	}
+  }
+ return c;
 } 
